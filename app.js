f4fa797591b033f63cc0ea9ec4dfa37453cbf797
avpfre01 <92304237+avpfre01@users.noreply.github.com>
2022-02-21 17:27:55 +0100

    'server'

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
 
+
+//Static files 
+app.use(express.static('public'));
+
+//Templating Engine
+app.set('views', "./src/views")
+app.set('view engine', 'ejs')
+
 app.get("/", function(req, res){
 
 })
